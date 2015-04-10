cc6ee43272d6bb2d26d36dba379efb2c8bbb8640
Andrey Soloviev <andrey.soloviev.s@gmail.com>
2015-04-11 00:01:57 +0200

    Added user profiles through Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :models
+  resources :movies
+
+  root to: "movies#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
