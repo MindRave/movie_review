d3ec187ea4f00dee1ce1018904021f723ee0af7a
Andrey Soloviev <andrey.soloviev.s@gmail.com>
2015-04-12 11:12:31 +0200

    Convert review ratings to stars

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :reviews
-  devise_for :users
-  devise_for :models
-  resources :movies
-  root to: "movies#index"
+    devise_for :models
+
+    devise_for :users
+    resources :movies do
+        resources :reviews, except: [:show,:index]
+    end
+
+    root to: "movies#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411231642) do
+ActiveRecord::Schema.define(version: 20150411232520) do
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20150411231642) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
